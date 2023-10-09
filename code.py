05b61769cde28a19337475418c5e232eba4ed1fd
Mrayo <81033344+xyresh@users.noreply.github.com>
2023-10-10 01:00:08 +0300

    Add files via upload

--- code.py
+++ code.py
@@ -15,15 +15,18 @@ from adafruit_hid.keycode import Keycode
 
 #declaration of Keys to send via USB to PC 
 #feel free to change the keys to your liking
-key_A=Keycode.C
-key_SPACE = Keycode.V
+key_A=Keycode.V
+key_SPACE = Keycode.C
 keyboard=Keyboard(usb_hid.devices)
 
 
 led = digitalio.DigitalInOut(board.LED)
+
 led.direction = digitalio.Direction.OUTPUT
+
 button1 = digitalio.DigitalInOut(board.GP21)
 button1.switch_to_input(pull=digitalio.Pull.DOWN)
+
 button2 = digitalio.DigitalInOut(board.GP22)
 button2.switch_to_input(pull=digitalio.Pull.DOWN)
 
@@ -35,13 +38,18 @@ button2.switch_to_input(pull=digitalio.Pull.DOWN)
 while True:
     if button1.value:
         led.value = True
-        keyboard.send(key_A)
-	time.sleep(0.5) #the sleep time seems to be optimum at 500ms providing good debounce time and springback time but of course this can be tweaked much more
-    led.value = False
-    
+        keyboard.press(key_A)
+        time.sleep(0.01)
+        led.value = False
     if button2.value:
         led.value = True
-        keyboard.send(key_SPACE)
-	time.sleep(0.5)
-    led.value = False
-    
+        keyboard.press(key_SPACE)
+        time.sleep(0.01)
+        led.value = False
+
+    if button1.value == False:
+        keyboard.release(key_A)
+
+    if button1.value == False:
+        keyboard.release(key_SPACE)
+
